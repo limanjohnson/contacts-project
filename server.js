2a01f116fa7335d0e8f10f07b2438817d1afa65b
Liam <liam@simplifybiz.com>
2025-05-12 10:25:26 -0600

    Through step 6

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 // web server
 const express = require('express');
+
+const mongodb = require('./data/database.js');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.use("/", require('./routes'));
 
-app.listen(port, () => {
-    console.log(`'Running on port ${port}'`)
-})
+mongodb.initDb((err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        app.listen(port, () => {
+            console.log(`'Running on port ${port}'`)
+        })
+    }
+});
+
